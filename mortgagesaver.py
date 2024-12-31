bb9375891feaacd2853dc01f37e1ddc29c7636e4
ChaseAlberti <chase.alberti.jy@renesas.com>
2024-12-30 21:38:37 -0600

    Reduced mortgage class into main function

--- mortgagesaver.py
+++ mortgagesaver.py
@@ -1,67 +1,7 @@
 import argparse
-import decimal
 
 MONTHS_IN_YEAR:int = 12
-DOLLAR_QUANTIZE = decimal.Decimal('.01')
 
-def dollar(f, round=decimal.ROUND_CEILING):
-    """
-    This function rounds the passed float to 2 decimal places.
-    """
-    if not isinstance(f, decimal.Decimal):
-        f = decimal.Decimal(str(f))
-    return f.quantize(DOLLAR_QUANTIZE, rounding=round)
-
-class Mortgage:
-    def __init__(self, interest, months, amount):
-        self._interest = float(interest)
-        self._months = int(months)
-        self._amount = dollar(amount)
-
-    def rate(self):
-        return self._interest
-
-    def month_growth(self):
-        return 1. + self._interest / MONTHS_IN_YEAR
-
-    def apy(self):
-        return self.month_growth() ** MONTHS_IN_YEAR - 1
-
-    def loan_years(self):
-        return float(self._months) / MONTHS_IN_YEAR
-
-    def loan_months(self):
-        return self._months
-
-    def amount(self):
-        return self._amount
-
-    def monthly_payment(self):
-        pre_amt = float(self.amount()) * self.rate() / (float(MONTHS_IN_YEAR) * (1.-(1./self.month_growth()) ** self.loan_months()))
-        return dollar(pre_amt, round=decimal.ROUND_CEILING)
-
-    def total_value(self, m_payment):
-        return m_payment / self.rate() * (float(MONTHS_IN_YEAR) * (1.-(1./self.month_growth()) ** self.loan_months()))
-
-    def annual_payment(self):
-        return self.monthly_payment() * MONTHS_IN_YEAR
-
-    def total_payout(self):
-        return self.monthly_payment() * self.loan_months()
-
-    def monthly_payment_schedule(self):
-        monthly = self.monthly_payment()
-        balance = dollar(self.amount())
-        rate = decimal.Decimal(str(self.rate())).quantize(decimal.Decimal('.000001'))
-        while True:
-            interest_unrounded = balance * rate * decimal.Decimal(1)/MONTHS_IN_YEAR
-            interest = dollar(interest_unrounded, round=decimal.ROUND_HALF_UP)
-            if monthly >= balance + interest:
-                yield balance, interest
-                break
-            principle = monthly - interest
-            yield principle, interest
-            balance -= principle
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("MortgageSaver", description="This program will calculate the difference between two interest rates and the amount of time it will take to payoff a refinance if closing costs are provided.")
@@ -74,8 +14,16 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    monthly_payment_initial: float = Mortgage(interest=args.initial / 100, amount=args.amount, months=args.term * MONTHS_IN_YEAR).monthly_payment()
-    monthly_payment_final: float = Mortgage(interest=args.final / 100, amount=args.amount, months=args.term * MONTHS_IN_YEAR).monthly_payment()
+    term_length_months = args.term * MONTHS_IN_YEAR
+
+    rate_initial: float = args.initial / 100.
+    rate_final: float = args.final / 100.
+
+    monthly_growth_initial: float = 1. + rate_initial / MONTHS_IN_YEAR
+    monthly_growth_final: float = 1. + rate_final / MONTHS_IN_YEAR
+
+    monthly_payment_initial: float = float(args.amount) * rate_initial / (float(MONTHS_IN_YEAR) * (1.-(1./monthly_growth_initial) ** term_length_months))
+    monthly_payment_final: float = float(args.amount) * rate_final / (float(MONTHS_IN_YEAR) * (1.-(1./monthly_growth_final) ** term_length_months))
     monthly_payment_difference: float = monthly_payment_initial - monthly_payment_final
 
     print("*** Monthly Payment ***")
